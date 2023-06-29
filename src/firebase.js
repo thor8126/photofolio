63c0da51286393e3e56fe6343df234ec5a997f0b
Lakhvinder <lakhabhayi@gmail.com>
2023-06-30 00:20:10 +0530

    css

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,21 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCcJB7wrBlLx7lPrPxG00JZqM1bBEQvXEM",
-  authDomain: "photofolio-d4f3f.firebaseapp.com",
-  projectId: "photofolio-d4f3f",
-  storageBucket: "photofolio-d4f3f.appspot.com",
-  messagingSenderId: "741535191397",
-  appId: "1:741535191397:web:5bd98c865c8b279335118c"
+  apiKey: "AIzaSyBIE6A5dTNNiCs2jvbOKmTsT27onzXpCms",
+  authDomain: "photofolio-603da.firebaseapp.com",
+  projectId: "photofolio-603da",
+  storageBucket: "photofolio-603da.appspot.com",
+  messagingSenderId: "700217801989",
+  appId: "1:700217801989:web:52ac51f2fbcbc9dd0a8b6c",
+  measurementId: "G-SPH1HV5V8T",
 };
 
 // Initialize Firebase
